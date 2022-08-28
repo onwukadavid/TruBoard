dbab9675dad44ecc6594f877385801c990c96754
Onwuka David <onwukadavid21@gmail.com>
2022-08-28 17:08:43 +0100

    Updated settings.py and added requirements.txt

--- truboards/settings.py
+++ truboards/settings.py
@@ -20,7 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f6bb12^#@$u^msqh#scgz8u979drrs43h+hbd#1$(g^bqe%^ti'
+from decouple import config, Csv
+import dj_database_url
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default = False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+DATABASES = {
+    'default': dj_database_url.config(
+        default = config('DATABASE_URL')
+    )
+}
+
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
