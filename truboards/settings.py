ac87bf1f4a6b93dc2f6000fb5cf6344d825ab4b0
Onwuka David <onwukadavid21@gmail.com>
2022-12-31 00:24:11 +0100

    added whitenoise

--- truboards/settings.py
+++ truboards/settings.py
@@ -165,7 +165,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
