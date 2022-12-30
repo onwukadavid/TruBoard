b16427a10188f48f59e299e31a2f873c84d62c0f
Onwuka David <onwukadavid21@gmail.com>
2022-12-30 14:58:06 +0100

    removed db comments

--- truboards/settings.py
+++ truboards/settings.py
@@ -101,10 +101,6 @@ WSGI_APPLICATION = 'truboards.wsgi.application'
 DATABASE_URL = config('DATABASE_URL')
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
 
     'default': dj_database_url.config(
         default = DATABASE_URL,
@@ -112,17 +108,6 @@ DATABASES = {
         # engine='django.db.backends.mysql',
     ),
 
-    #  'default': {  
-    #     'ENGINE': 'django.db.backends.mysql',  
-    #     'NAME': config('DB_NAME'),  
-    #     'USER': config('DB_USER'),  
-    #     'PASSWORD': config('DB_PASSWORD'),  
-    #     'HOST': config('DB_HOST'),  
-    #     'PORT': config('PORT_NUMBER'),  
-    #     'OPTIONS': {  
-    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-    #     }  
-    # }  
 }
 
 
