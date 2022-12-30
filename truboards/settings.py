a29366c87a91330da4b1f0b6a9db07f3dabb0c6c
Onwuka David <onwukadavid21@gmail.com>
2022-12-30 14:39:52 +0100

    using database url

--- truboards/settings.py
+++ truboards/settings.py
@@ -101,16 +101,16 @@ WSGI_APPLICATION = 'truboards.wsgi.application'
 DATABASE_URL = config('DATABASE_URL')
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 
-    # 'default': dj_database_url.config(
-    #     default = 'DATABASE_URL',
-    #     conn_max_age=1800,
-    #     engine='django.db.backends.mysql'
-    # ),
+    'default': dj_database_url.config(
+        default = DATABASE_URL,
+        conn_max_age=1800,
+        # engine='django.db.backends.mysql',
+    ),
 
     #  'default': {  
     #     'ENGINE': 'django.db.backends.mysql',  
