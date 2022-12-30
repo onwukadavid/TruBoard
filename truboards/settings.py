d6b0ac44b3b9996f6d19692d10520b354f008188
Onwuka David <onwukadavid21@gmail.com>
2022-12-30 11:43:13 +0100

    removed dot-env

--- truboards/settings.py
+++ truboards/settings.py
@@ -13,13 +13,12 @@ import os
 import django_heroku
 from pathlib import Path
 
-from dotenv import load_dotenv
 from decouple import config, Csv
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR, ".env"))
+# load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
