d2a08af9f1bab559ca031e826a7f3f4793b8b3fe
Onwuka David <onwukadavid21@gmail.com>
2022-12-31 00:40:21 +0100

    added static to urls

--- truboards/urls.py
+++ truboards/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 from boards import views
@@ -60,3 +62,6 @@ urlpatterns = [
 ]
 
 handler404 = "boards.views.page_not_found_view"
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATICFILES_DIRS)
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
