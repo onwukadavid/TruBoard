e413e0b3ce256aa6c4609bca4eeb96cedc70a68d
Onwuka David <onwukadavid21@gmail.com>
2022-12-30 02:45:55 +0100

    changed database

--- truboards/settings.py
+++ truboards/settings.py
@@ -97,26 +97,26 @@ WSGI_APPLICATION = 'truboards.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
     # 'default': dj_database_url.config(
     #     default = config('DATABASE_URL')
     # )
 
-     'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': config('DB_NAME'),  
-        'USER': config('DB_USER'),  
-        'PASSWORD': config( 'DB_PASSWORD'),  
-        'HOST': config('DB_HOST'),  
-        'PORT': config('PORT_NUMBER'),  
-        'OPTIONS': {  
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-        }  
-    }  
+    #  'default': {  
+    #     'ENGINE': 'django.db.backends.mysql',  
+    #     'NAME': config('DB_NAME'),  
+    #     'USER': config('DB_USER'),  
+    #     'PASSWORD': config( 'DB_PASSWORD'),  
+    #     'HOST': config('DB_HOST'),  
+    #     'PORT': config('PORT_NUMBER'),  
+    #     'OPTIONS': {  
+    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+    #     }  
+    # }  
 }
 
 
